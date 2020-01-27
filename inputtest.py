c6e665d833faee1ec2891bfb9d9eecc7ac37a0da
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2020-01-27 20:27:26 +0800

    simple calculator and input test

--- inputtest.py
+++ inputtest.py
@@ -4,11 +4,15 @@ root = Tk()
 '''
 https://www.youtube.com/watch?v=YXPyB4XeYLA
 '''
+e = Entry(root, width=50, bg="blue", fg="white", borderwidth=0.5)
+e.pack()
+e.insert(0, "Enter Your Name: ")
 
 def myClick():
-    myLabel = Label(root, text="Look, I clicked a button!")
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
 
-myButton = Button(root, text="Click Me!", state=NORMAL, command=myClick, fg= '#ffffaa', bg='green') # Don't type parentheses in the command buttons
+myButton = Button(root, text="Enter Your Name", state=NORMAL, command=myClick, fg= '#ffffaa', bg='green') # Don't type parentheses in the command buttons
 myButton.pack()
 root.mainloop()
